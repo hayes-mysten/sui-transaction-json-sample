f3a3cdbf4f6fb9ef3591a20324ee9239a154e445
Michael Hayes <michael.hayes@mystenlabs.com>
2024-06-03 12:44:48 -0700

    Add more samples

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-import { Transaction, Inputs } from '@mysten/sui/transactions'
+import { Transaction, Inputs, coinWithBalance } from '@mysten/sui/transactions'
+import { requestSuiFromFaucetV0, getFaucetHost } from '@mysten/sui/faucet'
+import { Ed25519Keypair } from '@mysten/sui/keypairs/ed25519'
 import { toB58 } from '@mysten/bcs'
+import { promises } from 'fs'
+import { SuiClient, getFullnodeUrl } from '@mysten/sui/client'
+import { decodeSuiPrivateKey } from '@mysten/sui/cryptography'
 
+const client = new SuiClient({ url: getFullnodeUrl('devnet') })
+const sender = Ed25519Keypair.fromSecretKey(decodeSuiPrivateKey('suiprivkey1qpakl9hglralt2qml5yzy90mq9mrv2vfld8hxpwllmgl4nwdttkcsqgjer8').secretKey);
+
+await requestSuiFromFaucetV0({ host: getFaucetHost('devnet'), recipient: sender.toSuiAddress() })
 
 const tx = new Transaction();
 
-tx.moveCall({
+const result = tx.moveCall({
     target: '0x2::foo::bar',
     arguments: [
         tx.object('0x123'),
@@ -33,8 +42,10 @@ tx.moveCall({
     ],
 });
 
-tx.splitCoins(tx.gas, [1])
-tx.transferObjects([tx.gas], '0x2')
+
+
+const [coin] = tx.splitCoins(tx.gas, [1])
+tx.transferObjects([coin, result], '0x2')
 tx.makeMoveVec({
     elements: [
         tx.object('4')
@@ -55,10 +66,25 @@ tx.publish({
 })
 
 tx.setGasBudget(10_000_000)
-tx.setSender('0x123')
+tx.setSender(sender.toSuiAddress())
 
 const jsonv2 = await tx.toJSON();
 
-console.log(jsonv2)
+await promises.writeFile('sample.json', jsonv2)
+
+
+const withIntent = new Transaction();
+withIntent.setSender(sender.toSuiAddress())
+withIntent.transferObjects([coinWithBalance({ balance: 100n})], sender.toSuiAddress())
+
+const unresolvedJSON = await withIntent.toJSON({ supportedIntents: ['CoinWithBalance'] })
+
+await promises.writeFile('unresolved-intent.json', unresolvedJSON)
+
+const resolvedJSON = await withIntent.toJSON({ client, supportedIntents: [] })
+
+await promises.writeFile('resolved-intent.json', resolvedJSON)
+
+
 
 
